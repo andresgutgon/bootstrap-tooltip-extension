1ffc2a411c3b6009a594c9f02844bb18d70c97a6
Andres Gutierrez <andres@teambox.com>
2013-05-22 17:01:03 +0200

    Add code file, update README and add example

--- README.md
+++ README.md
@@ -1,15 +1,64 @@
 ## Twitter Bootstrap tooltip extension
-<a href="http://teambox.com"><img alt="Built at Teambox" src="http://i.imgur.com/hqNPlHe.png"/></a>
 
-With this plugin extension I try to get 4 extra position for twiiter bootstrap tooltip: `top-left`, `top-right`, `bottom-left` and `bottom-right`.
+With this plugin extension I try to get **4 extra positions** for twiiter bootstrap tooltip: `top-left`, `top-right`, `bottom-left` and `bottom-right`.
 
-### Top left
-![top left](https://github.com/andresgutgon/bootstrap-tooltip-extension/example/images/top-left.png)
+### Example
+![top left](https://raw.github.com/andresgutgon/bootstrap-tooltip-extension/master/example/images/top-left.png)
+
 
 ### Usage
 If you want to use you have to add this file just after bootstrap tooltip. And **before** bootstrap popovers
 
 ```html
-  <script src="assets/js/bootstrap-tooltip.js"></script>
-  <script src="assets/js/bootstrap-tooltip-extension.js"></script>
+  <script src="bootstrap-tooltip.js"></script>
+  <script src="bootstrap-tooltip-extension.js"></script>
 ```
+
+In your html:
+``` html
+  <h4 id="top-left" data-toggle="tooltip" data-placement="top-left" data-original-title="Tooltip on top left">Top Left</h4>
+  <script type="text/javascript">
+    $('#top-left').tooltip('show');
+  </script>
+```
+
+## css to be added
+This is the **css** with this 4 extra positions
+
+``` css
+.tooltip.top-left .tooltip-arrow {
+  bottom: -5px;
+  left: 5px;
+  border-width: 5px 5px 0;
+  border-top-color: black;
+}
+.tooltip.top-right .tooltip-arrow {
+  bottom: -5px;
+  right: 5px;
+  border-width: 5px 5px 0;
+  border-top-color: black;
+}
+.tooltip.bottom-left .tooltip-arrow {
+  top: -5px;
+  left: 5px;
+  border-width: 0 5px 5px;
+  border-bottom-color: black;
+}
+.tooltip.bottom-right .tooltip-arrow {
+  top: -5px;
+  right: 5px;
+  border-width: 0 5px 5px;
+  border-bottom-color: black;
+}
+```
+
+### License
+The MIT License (MIT)
+
+Copyright (c) 2013 Pedro  p3dro.sola@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ bootstrap-tooltip-extension.js
@@ -0,0 +1,140 @@
+/* ===========================================================
+ * bootstrap-tooltip-extension.js
+ * https://github.com/andresgutgon/bootstrap-tooltip-extension
+ * ===========================================================
+ *
+ * This file extends bootstrap-tooltip.js that add
+ * More tooltip positions 'bottom-left', 'bottom-right', 'top-left' and 'top-right'
+ *
+ * ATENTION:
+ * This file is ours, but must be in vendor to be defined before bootstrap-popover.js
+ *
+ * =========================================================== */
+
+(function ($) {
+  var old
+    , TooltipExtension = function (element, options) {
+    this.init('tooltip', element, options);
+  };
+
+  TooltipExtension.prototype = $.extend({}, $.fn.tooltip.Constructor.prototype, {
+    constructor: TooltipExtension
+  , show: function () {
+      var $tip
+        , pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , tp
+        , e = $.Event('show');
+
+      if (this.hasContent() && this.enabled) {
+        this.$element.trigger(e);
+        if (e.isDefaultPrevented()) { return; }
+        $tip = this.tip();
+        this.setContent();
+
+        if (this.options.animation) {
+          $tip.addClass('fade');
+        }
+
+        placement = typeof this.options.placement === 'function' ?
+          this.options.placement.call(this, $tip[0], this.$element[0]) :
+          this.options.placement;
+
+        $tip
+          .detach()
+          .css({ top: 0, left: 0, display: 'block' });
+
+        if (this.options.container) {
+          $tip.appendTo(this.options.container);
+        } else {
+          $tip.insertAfter(this.$element);
+        }
+
+        pos = this.getPosition();
+
+        actualWidth = $tip[0].offsetWidth;
+        actualHeight = $tip[0].offsetHeight;
+
+        switch (placement) {
+        case 'bottom':
+          tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
+          break;
+        case 'top':
+          tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
+          break;
+        case 'left':
+          tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
+          break;
+        case 'right':
+          tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+          break;
+        case 'right':
+          tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+          break;
+        // Extra positions. This are not part of bootstrap
+        // Extrange:
+        //  I've to make top position 10px smaller in 'top-left' and 'top-right'
+        //  And 10px bigger in 'bottom-left' and 'bottom-right'
+        // This should behave like 'top' and 'bottom'. But they don't.
+        case 'bottom-left':
+          tp = {top: pos.top + pos.height + 10, left: pos.left};
+          break;
+        case 'bottom-right':
+          tp = {top: pos.top + pos.height + 10, left: pos.left + pos.width - actualWidth};
+          break;
+        case 'top-left':
+          tp = {top: pos.top - actualHeight - 10, left: pos.left};
+          break;
+        case 'top-right':
+          tp = {top: pos.top - actualHeight - 10, left: pos.left + pos.width - actualWidth};
+          break;
+        }
+
+        this.applyPlacement(tp, placement);
+        this.$element.trigger('shown');
+      }
+    }
+  });
+
+
+
+ /* TOOLTIP EXTRA PLUGIN DEFINITION
+  * ========================= */
+
+  old = $.fn.tooltip;
+
+  $.fn.tooltip = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('tooltip')
+        , options = typeof option === 'object' && option;
+      if (!data) { $this.data('tooltip', (data = new TooltipExtension(this, options))); }
+      if (typeof option === 'string') { data[option](); }
+    });
+  };
+
+  $.fn.tooltip.Constructor = TooltipExtension;
+
+  $.fn.tooltip.defaults = {
+    animation: true
+  , placement: 'top'
+  , selector: false
+  , template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+  , trigger: 'hover focus'
+  , title: ''
+  , delay: 0
+  , html: false
+  , container: false
+  };
+
+ /* TOOLTIP EXTRA NO CONFLICT
+  * =================== */
+
+  $.fn.tooltip.noConflict = function () {
+    $.fn.tooltip = old;
+    return this;
+  };
+
+})(window.jQuery);
